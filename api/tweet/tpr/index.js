318284da65c1dc643fe44e84ac615acb600b4973
Fernando Parra <fernandoxe@gmail.com>
2021-09-05 00:19:28 -0300

    chore: used refactored tweet

--- api/tweet/tpr/index.js
+++ api/tweet/tpr/index.js
@@ -1,7 +1,7 @@
 const tpr = require('../../../data/tpr');
 const config = require('../../../.config');
 const Twit = require('twit');
-const { tweet } = require('../../../src/tweet');
+const { postTweet } = require('../../../src/tweet');
 
 const T = new Twit({
   consumer_key: config.tpr.consumer_key,
@@ -10,6 +10,11 @@ const T = new Twit({
   access_token_secret: config.tpr.access_token_secret,
 });
 
-module.exports = (req, res) => {
-  tweet(tpr, T, res);
+module.exports = async (req, res) => {
+  try {
+    const response = await postTweet(tpr, T);
+    res.status(200).send(response);
+  } catch(error) {
+    res.status(500).send(`Error: ${error.message} (${error.code})`);
+  }
 };
