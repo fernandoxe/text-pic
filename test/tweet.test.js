f36e050befed9f4ee9a66dc1b76512b70dfcef44
Fernando Parra <fernandoxe@gmail.com>
2021-09-07 00:58:53 -0300

    chore: test postTweet

--- test/tweet.test.js
+++ test/tweet.test.js
@@ -1,20 +1,24 @@
 const expect = require('chai').expect;
 const path = require('path');
 const { createCanvas, registerFont } = require('canvas');
-const { getFitLines } = require('../src/tweet');
+const { getFitLines, postTweet } = require('../src/tweet');
 
 describe('Tweet test', () => {
-  it('Should return lines that fit in canvas', () => {
-    const width = 1080;
-    const height = 1440;
-    const horizontalMargin = 60;
-    const maxTextWidth = width - horizontalMargin;
-    const fontSize = 80;
+  const width = 1080;
+  const height = 1440;
+  const horizontalMargin = 60;
+  const maxTextWidth = width - horizontalMargin;
+  const fontSize = 80;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+  ctx.font = `${fontSize}px 'Glory'`;
+  ctx.textAlign = 'center';
+
+  before(() => {
     registerFont(path.join(__dirname, '../src/fonts/CormorantGaramond-Bold.ttf'), {family: 'Glory'});
-    const canvas = createCanvas(width, height);
-    const ctx = canvas.getContext('2d');
-    ctx.font = `${fontSize}px 'Glory'`;
-    ctx.textAlign = 'center';
+  });
+
+  it('Should return lines that fit in canvas', () => {
     const tweet = 'Show me all the things that I shouldn\'t know\nAnd there\'s a blue moon on the rise';
     const linesMock = [
       'Show me all the things that I',
@@ -25,6 +29,26 @@ describe('Tweet test', () => {
 
     const lines = getFitLines(tweet, ctx, maxTextWidth);
 
-    expect(lines).to.eql(linesMock);
+    expect(lines).to.be.eql(linesMock);
+  });
+
+  it('Should post the tweet', async () => {
+    const data = {
+      name: 'tpr',
+      tweets: [
+        'Somebody mixed my medicine',
+        'Well, you hurt where you sleep and you sleep where you lie',
+        'And now you\'re in deep and now you\'re gonna cry',
+        'You start to sweat so hold me tight',
+      ],
+      images: [
+        'taylor1',
+        'taylor2',
+      ]
+    };
+    
+    const response = await postTweet(data, null);
+
+    expect(response).to.be.a('string');
   });
 });
